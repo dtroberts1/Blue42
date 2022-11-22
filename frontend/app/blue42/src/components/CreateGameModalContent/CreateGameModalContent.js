11bacd1ee8aa0d39407ac3b6f044d5a5e8b754ff
Dylan Roberts <dtroberts@knights.ucf.edu>
2022-11-21 19:57:01 -0500

    Created initial generic image-selector-list

--- frontend/app/blue42/src/components/CreateGameModalContent/CreateGameModalContent.js
+++ frontend/app/blue42/src/components/CreateGameModalContent/CreateGameModalContent.js
@@ -2,12 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './CreateGameModalContent.less';
 import styles from './CreateGameModalContent.less';
+import acImage from '../../assets/team-icons/AC.png';
+import ReactTooltip from 'react-tooltip';
 
-const CreateGameModalContent = () => (
-  <div className={styles.CreateGameModalContent}>
-    CreateGameModalContent Component
-  </div>
-);
+const teams = [
+  'New York Patriots',
+  'New York Jets',
+  'Tampa Bay Bucs',
+  'New York Giants',
+  'Washington Redskins',
+  'Miami Dolphins',
+  'San Francisco 49ers'
+]
+
+class CreateGameModalContent extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      teamList: this.teamListItems()
+    }
+  }
+
+  teamListItems(params) {
+    return teams.map((team) => 
+    <li className={styles.ImgSelectorItem}>
+      <div className={styles.ImgSelectorItemContainer}>
+        <div className={styles.ImgSelectorItemImg}><img src={acImage}/></div>
+        <div className={styles.ImgSelectorItemValue}><div data-tip={team} data-class={styles.ToolTipClass}>{team} <ReactTooltip /></div></div> 
+      </div>
+    </li>
+  );
+  }
+
+  render(){
+    return(
+      <div className={styles.CreateGameModalContent}>
+      CreateGameModalContent Component
+      <div className={styles.ImgSelectorContainer}>
+        <ul className={styles.ImgSelectorList}>
+          {this.state.teamList}
+        </ul>
+      </div>
+    </div>
+    );
+  }
+}
 
 CreateGameModalContent.propTypes = {};
 
